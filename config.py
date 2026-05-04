d6d3c47809e53dc1e6d0e57022776931c01d43ee
thiendang184-droid <thiendang184@gmail.com>
2026-05-04 23:04:46 +0700

    Remove comments from config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# config.py
-MAPBOX_TOKEN = "pk.eyJ1IjoiZGF6ZTcxMCIsImEiOiJjbW9wZHc5Ym0wM2tuMnJxM2pndnZvcWV5In0.O5mzEorw1pZQJ2JdRx7JUg"        # ← Thay bằng token thật của bạn
 
-OPENWEATHERMAP_API_KEY = "2a8575dc1f264a8b11dfc51d9965ec38"            # Thay key OpenWeatherMap vào (có thể để trống tạm)
+MAPBOX_TOKEN = "pk.eyJ1IjoiZGF6ZTcxMCIsImEiOiJjbW9wZHc5Ym0wM2tuMnJxM2pndnZvcWV5In0.O5mzEorw1pZQJ2JdRx7JUg"       
+
+OPENWEATHERMAP_API_KEY = "2a8575dc1f264a8b11dfc51d9965ec38"          
